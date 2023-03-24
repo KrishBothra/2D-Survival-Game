969ceb8eae805306cc7b8266bf7e32839e4f85d1
DumplingDefender <82426255+DumplingDefender@users.noreply.github.com>
2023-03-24 12:35:17 -0500

    basic setup#1

--- src/main/java/com/example/template/HelloController.java
+++ src/main/java/com/example/template/HelloController.java
@@ -4,16 +4,53 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class HelloController {
     @FXML
     private Label lbl;
+    @FXML
+    private GridPane gPane;
 
     @FXML
     private TextField txtField;
 
+    ImageView[][] img;
+
+    Image grass;
+
+    FileInputStream grasss;
+
+    public HelloController(){
+
+        try {
+            grasss = new FileInputStream("");
+
+
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+
     @FXML
     private void onClick(){
+
+        for(int i = 0; i<img.length; i++){
+            for (int j = 0; j < img[0].length; j++) {
+                img[i][j] = new ImageView();
+                img[i][j].setImage(back);
+                img[i][j].setFitHeight(125);
+                img[i][j].setFitWidth(125);
+                img[i][j].setRotate(90);
+                gPane.add(img[i][j], j, i);
+            }
+        }
+
         System.out.println("hello");
         System.out.println("i am at home");
 
