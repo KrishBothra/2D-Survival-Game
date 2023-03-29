5cebfade7b16e1a4b6460257738a8c66ebeebbf0
DumplingDefender <82426255+DumplingDefender@users.noreply.github.com>
2023-03-29 18:18:29 -0500

    this is a test

--- src/main/java/com/example/template/HelloController.java
+++ src/main/java/com/example/template/HelloController.java
@@ -50,11 +50,8 @@ public class HelloController {
             }
         }
 
-        System.out.println("hello");
-        System.out.println("i am at home");
-        System.out.println("danny is at home");
-        System.out.println("danny is a poo poo");
-        System.out.println("test test test");
+
+
     }
 
 }
