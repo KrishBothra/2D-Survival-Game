289846024cf18d15ac4139807fc4ec240743aec7
DumplingDefender <82426255+DumplingDefender@users.noreply.github.com>
2023-03-29 19:36:00 -0500

    basic map and movement

--- src/main/java/com/example/template/HelloController.java
+++ src/main/java/com/example/template/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.template;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -20,8 +21,22 @@ public class HelloController {
     @FXML
     private TextField txtField;
 
-    ImageView[][] img = new ImageView[4][4];
+    private int x = 25;
+
+    private int y = 41;
+
+    ImageView[][] img = new ImageView[x][y];
+
+    String[][] map = new String[x*8+1][y*8+1]; //100 //164
     Image grass;
+
+//    int changeX = 0;
+//    int changeY = 0;
+
+    int playerPositionX = 99;//-1
+    int playerPositionY = 163;//-1
+
+    //12,20----13,21
 
     FileInputStream grasss;
 
@@ -43,22 +58,52 @@ public class HelloController {
             for (int j = 0; j < img[0].length; j++) {
                 img[i][j] = new ImageView();
                 img[i][j].setImage(grass);
-                img[i][j].setFitHeight(125);
-                img[i][j].setFitWidth(125);
-                img[i][j].setRotate(90);
+                img[i][j].setFitHeight(40);
+                img[i][j].setFitWidth(40);
                 gPane.add(img[i][j], j, i);
             }
         }
 
+        for(int i = 0; i<map.length; i++){
+            for (int j = 0; j < map[0].length; j++) {
+                map[i][j] = "grass";
+            }
+        }
+
+//        img[12][20].setImage(null);
+        map[98][160] = "null";
+        map[99][163] = "null";
+
+
+        gPane.setGridLinesVisible(true);
+
+        updateScreen();
 
        
 
     }
 
+    private void updateScreen(){
+        for(int i = 0; i< img.length; i++){
+            for (int j = 0; j < img[0].length; j++) {
+                if(map[playerPositionX-12+i][playerPositionY-20+j].equals("grass")){
+                    img[i][j].setImage(grass);
+                }
+                else if(map[playerPositionX-12+i][playerPositionY-20+j].equals("null")){
+                    img[i][j].setImage(null); //steve
+                }
+            }
+        }
 
 
-    private void thisisanother(){
-        System.out.println("thingsandshit");
     }
 
+
+    public void onClick2(ActionEvent event) {
+        map[playerPositionX][playerPositionY] = "grass";
+        playerPositionX -=2;
+        playerPositionY -= 5;
+        map[playerPositionX][playerPositionY] = "null";
+        updateScreen();
+    }
 }
